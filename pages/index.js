c9a8ed843bc8c17a6a9e2e414c55809e051e66cb
mohitS <mohitsrivastava67@gmail.com>
2021-06-23 00:00:25 +0530

    init: added meta tags and cleared boilerplate

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,57 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Mohit's Blog</title>
+        <link rel="canonical" href="https://mohits.dev/" />
+        <meta name="title" content="Mohit's Blog" />
+        <meta
+          name="description"
+          content="Mohit is a software engineer. He writes about JavaScript, TypeScript, Node.JS, CSS, GraphQL and some C++"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Mohit's Blog" />
+        <meta property="og:url" content="https://mohits.dev/" />
+        <meta property="og:title" content="Mohit's Blog" />
+        <meta
+          property="og:description"
+          content="Mohit is a software engineer. He writes about JavaScript, TypeScript, Node.JS, CSS, GraphQL and some C++"
+        />
+        <meta
+          property="og:image"
+          content="https://avatars.githubusercontent.com/u/17087942?v=4"
+        />
+        <meta property="og:image:width" content="600" />
+        <meta property="og:image:height" content="600" />
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:url" content="https://mohits.dev/" />
+        <meta property="twitter:title" content="Mohit's Blog" />
+        <meta
+          property="twitter:description"
+          content="Mohit is a software engineer. He writes about JavaScript, TypeScript, Node.JS, CSS, GraphQL and some C++"
+        />
+        <meta
+          property="twitter:image"
+          content="https://avatars.githubusercontent.com/u/17087942?v=4"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
+        <h1 className={styles.title}>Welcome to My Blog!</h1>
+        <p className={styles.description}>W.I.P.</p>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/msx47/blog"
           target="_blank"
-          rel="noopener noreferrer"
+          referrerPolicy="no-referrer"
+          className={styles.link}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Github
         </a>
-      </footer>
+      </main>
     </div>
-  )
+  );
 }
