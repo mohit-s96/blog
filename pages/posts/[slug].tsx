5d9b428436bde01feffe46c8ddf9304665d7b181
msx47 <mohitsrivastava67@gmail.com>
2021-09-07 19:24:22 +0530

    testing: github actions env variables

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -7,17 +7,37 @@ import {
   fetchPathData,
 } from "../../lib/getBlogs";
 import { addBlog, addComment } from "../../lib/updateBlogData";
+import { BlogSlug } from "../../types/blogtypes";
 
 type Props = {
-  str: string;
+  slug: BlogSlug;
 };
 
-const Post = ({}: Props) => {
+const Post = ({ slug: { author, excerpt, likes, commentCount } }: Props) => {
   const router = useRouter();
   // if (!router.isFallback /*&& !post?.slug*/) {
   //   return <ErrorPage statusCode={404} />;
   // }
-  return <div>Some blog page</div>;
+  return (
+    <div className="text-center grid place-items-center p-4">
+      <h1 className="p-3">
+        <b>Author:</b>
+      </h1>
+      <p className="p-3 mb-2 text-blue-700">{author}</p>
+      <h1 className="p-3">
+        <b>Excerpt:</b>
+      </h1>
+      <p className="p-3 mb-2 text-blue-700 w-7/12">{excerpt}</p>
+      <h1 className="p-3">
+        <b>Likes:</b>
+      </h1>
+      <p className="p-3 mb-2 text-blue-700">{likes}</p>
+      <h1 className="p-3">
+        <b>Comments:</b>
+      </h1>
+      <p className="p-3 mb-2 text-blue-700">{commentCount}</p>
+    </div>
+  );
 };
 
 export default Post;
@@ -29,13 +49,10 @@ type Params = {
 };
 
 export async function getStaticProps(x: Params) {
-  // const data = await fetchComments("61360f8909f47f1c62bb188a");
-  // console.log(data);
-  // let y = await addComment();
-  // console.log(y);
-
+  const data = await fetchSingleBlog("figma_design");
+  (data._id as any) = data._id.toHexString();
   return {
-    props: { str: "hello" },
+    props: { slug: data },
   };
 }
 
