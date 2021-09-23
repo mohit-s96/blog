055fb43a802766eedbc3dbb46edceeaf5bc717c7
msx47 <mohitsrivastava67@gmail.com>
2021-09-23 20:57:37 +0530

    added: middleware creator and cors

--- src/pages/api/list/index.ts
+++ src/pages/api/list/index.ts
@@ -1,8 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
+import initMiddleware from "../../../../lib/middleware";
 import { fetchBlogList } from "../../../../lib/getBlogs";
 
-const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
+// Initialize the cors middleware
+const cors = initMiddleware(
+  Cors({
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
+    await cors(req, res);
     const data = await fetchBlogList();
     res.status(200).json(data);
   } catch (err) {
