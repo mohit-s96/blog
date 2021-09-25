e5316f83d8ba9fc43100a2d4945eb9772c8d6d60
msx47 <mohitsrivastava67@gmail.com>
2021-09-25 19:06:00 +0530

    added: route for revalidatiing user

---
+++ src/pages/api/rfrt/index.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import Cookies from "cookies";
+import Cors from "cors";
+import initMiddleware from "../../../../lib/middleware";
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  Cors({
+    methods: ["GET", "POST", "OPTIONS"],
+    origin: "http://localhost:3001",
+    credentials: true,
+  })
+);
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    await cors(req, res);
+    const cookies = new Cookies(req, res);
+    const str = cookies.get("token");
+    console.log(str);
+    if (str === process.env.SECRET) {
+      res.status(200).json({ message: "success" });
+    } else {
+      throw new Error("Unauthorized");
+    }
+  } catch (err) {
+    res.status(400).json({ error: (err as any).message });
+  }
+};
+
+export default handler;
