ab340ca327a91ea23b96f06971a87985e8709a05
msx47 <mohitsrivastava67@gmail.com>
2021-10-19 00:51:45 +0530

    added: responsive images

--- src/components/card/PostPreviewImage.tsx
+++ src/components/card/PostPreviewImage.tsx
@@ -1,23 +1,24 @@
 import React, { ReactElement } from "react";
+import { NewImageData } from "../../../types/blogtypes";
 import { LayoutType } from "../../../types/globalTypes";
+import ResImage from "./resImages";
 
 interface Props {
   type: LayoutType;
-  uri: string;
-  alt: string;
+  images: NewImageData;
 }
 
-function PostPreviewImage({ type, uri, alt }: Props): ReactElement {
+function PostPreviewImage({ type, images }: Props): ReactElement {
   return (
     <div className={`${type === "horiz" ? "w-card-lg-vert" : "w-full h-3/6"}`}>
-      <img
-        src={uri}
-        alt={alt}
-        className={`w-full h-full ${
+      <ResImage
+        className={`w-full ${
           type === "horiz"
             ? "rounded-tl-3xl rounded-bl-3xl"
             : "rounded-tl-3xl rounded-tr-3xl"
         }`}
+        alt={images.alt}
+        uris={images.permUri}
       />
     </div>
   );
