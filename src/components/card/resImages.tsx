6672c19efdaea1db6bd348f7f91c13e92c670563
msx47 <mohitsrivastava67@gmail.com>
2021-10-20 14:42:27 +0530

    changed: undo image resize change, now serves proper sizes

--- src/components/card/resImages.tsx
+++ src/components/card/resImages.tsx
@@ -9,8 +9,8 @@ interface Props {
 function ResImage({ uris, alt, className = "" }: Props): ReactElement {
   return (
     <picture>
-      <source media="(max-width: 1999px)" srcSet={uris[1].data?.Key} />
-      <source media="(min-width: 2000px)" srcSet={uris[2].data?.Key} />
+      <source media="(max-width: 799px)" srcSet={uris[1].data?.Key} />
+      <source media="(min-width: 800px)" srcSet={uris[2].data?.Key} />
       <img
         src={uris[2].data?.Key}
         alt={alt}
