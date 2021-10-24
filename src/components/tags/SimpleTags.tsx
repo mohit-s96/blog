71b2149f425cff9925c46cde3e2c0c1337952f53
msx47 <mohitsrivastava67@gmail.com>
2021-10-24 20:03:03 +0530

    changed: extended util component style with className prop

--- src/components/tags/SimpleTags.tsx
+++ src/components/tags/SimpleTags.tsx
@@ -4,12 +4,13 @@ import { ThemeType } from "../../../types/globalTypes";
 interface Props {
   tag: string;
   theme?: ThemeType;
+  className?: string;
 }
 
-function SimpleTags({ tag, theme }: Props): ReactElement {
+function SimpleTags({ tag, className = "" }: Props): ReactElement {
   return (
     <button
-      className={`p-1 m-1 rounded-sm bg-primary-accent-dark text-primary-text-dark`}
+      className={`p-1 m-1 rounded-sm bg-primary-accent-dark text-primary-text-dark ${className}`}
     >
       {"#" + tag}
     </button>
