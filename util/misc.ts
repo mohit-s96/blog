35c6a6ca5e1a50ed996f93970002e5d0f45b7558
msx47 <mohitsrivastava67@gmail.com>
2021-11-20 23:53:13 +0530

    added: helper functions to promisify redis get and set calls

--- util/misc.ts
+++ util/misc.ts
@@ -1,1 +1,34 @@
-export default 42069;
+import { RedisClient } from "redis";
+
+export function setRedisKey(
+  client: RedisClient,
+  key: string,
+  time: number,
+  value: string
+) {
+  return new Promise((resolve, reject) => {
+    client.setex(key, time, value, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+export function getRedisKey(client: RedisClient, key: string) {
+  return new Promise((resolve, reject) => {
+    client.get(key, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+export function transformRedisKey(key: string) {
+  return key.split(" ").join("-");
+}
