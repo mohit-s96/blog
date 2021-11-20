a701bb57fb639bf1b1d8c392289dbd88f4f0afca
msx47 <mohitsrivastava67@gmail.com>
2021-11-20 23:52:28 +0530

    added: method to retrieve blogs related to a provided key

--- lib/database/getBlogs.ts
+++ lib/database/getBlogs.ts
@@ -110,3 +110,27 @@ export function fetchBlogList() {
     }
   });
 }
+
+export function fetchSearchQuery(key: string) {
+  return dbConnect<Array<Partial<BlogSlug>>>(async (client) => {
+    try {
+      await client.connect();
+      const cursors = client
+        .db()
+        .collection(process.env.BLOG_COLLECTION as string)
+        .find(
+          { $text: { $search: key } },
+          { projection: { title: 1, uri: 1, tags: 1, createdAt: 1 } }
+        );
+      const blogs = (await cursors.toArray()) as Array<Partial<BlogSlug>>;
+      return blogs;
+    } catch (e) {
+      console.error(e);
+      return Promise.reject(
+        "Something went wrong. Error => " + JSON.stringify(e)
+      );
+    } finally {
+      await client.close();
+    }
+  });
+}
