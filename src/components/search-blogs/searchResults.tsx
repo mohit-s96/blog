85cb439287154677bd3fdba0fc74fcbb84bb0ea3
msx47 <mohitsrivastava67@gmail.com>
2021-11-21 23:47:59 +0530

    changed: removed hardcoded data and adjusted for dynamic data

--- src/components/search-blogs/searchResults.tsx
+++ src/components/search-blogs/searchResults.tsx
@@ -1,37 +1,36 @@
+import { format } from "date-fns";
 import React, { ReactElement } from "react";
+import { BlogSlug } from "../../../types/blogtypes";
 import { ThemeType } from "../../../types/globalTypes";
 import Tags from "../tags/Tags";
 
 interface Props {
-  theme: ThemeType;
+  data: Partial<BlogSlug>;
 }
 
-function SearchResults({ theme }: Props): ReactElement {
+function SearchResults({ data }: Props): ReactElement {
   return (
     <div
       className={`p-2 m-2 flex flex-col w-[98%] mx-auto rounded-2xl dark:bg-primary-bg-dark bg-primary-bg-light`}
     >
       <div className="flex justify-between">
         <p className="p-2 text-xl text-primary-accent-dark text-sm-res padding-0-res">
-          designing in figma
+          {data.title}
         </p>
         <p className="p-2 text-xl text-primary-accent-dark text-sm-res padding-0-res">
-          5 minute read
+          {format(data.createdAt as number, "do MMM, yy")}
         </p>
       </div>
       <div className="flex">
-        <Tags
-          children="#css"
-          accent
-          variant="sm"
-          className="mr-2 font-bold p-4 padding-0-res"
-        />
-        <Tags
-          children="#figma"
-          accent
-          variant="sm"
-          className="mr-2 font-bold p-4 padding-0-res"
-        />
+        {data.tags?.slice(0, 2).map((tag) => (
+          <Tags
+            key={tag}
+            children={`#${tag}`}
+            accent
+            variant="sm"
+            className="mr-2 font-bold p-4 padding-0-res"
+          />
+        ))}
       </div>
     </div>
   );
