92d918b10d416ea4caf283be2ea0b5d0b995f682
msx47 <mohitsrivastava67@gmail.com>
2021-11-21 23:43:26 +0530

    changed: main /search api route is useless so it is now a protected route

--- src/pages/api/search/index.ts
+++ src/pages/api/search/index.ts
@@ -1,42 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { fetchSearchQuery } from "../../../../lib/database/getBlogs";
 import { useCors } from "../../../../lib/middleware/corsMW";
-import redis from "redis";
-import {
-  getRedisKey,
-  setRedisKey,
-  transformRedisKey,
-} from "../../../../util/misc";
+import { useAuth } from "../../../../lib/middleware/authMW";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     await useCors(req, res);
 
-    const client = redis.createClient(
-      process.env.REDIS_ENDPOINT_URI as string,
-      {
-        password: process.env.REDIS_PASSWORD,
-      }
-    );
+    await useAuth(req, res);
 
-    const key = req.body.key as string;
-
-    const cached = await getRedisKey(client, transformRedisKey(key));
-
-    if (cached === null) {
-      const response = await fetchSearchQuery(key);
-      await setRedisKey(
-        client,
-        transformRedisKey(key),
-        345600,
-        JSON.stringify(response)
-      );
-      res.status(200).json(response);
-    } else {
-      res.status(200).json(cached);
-    }
+    res.status(200).json({ message: "hello" });
   } catch (err) {
-    res.status(500).json({ statusCode: 500, message: (err as any).message });
+    let code = 500;
+    if ((err as any)?.message === "Unauthorized") {
+      code = 400;
+    }
+    res.status(code).json({ statusCode: code, message: (err as any).message });
   }
 };
 
