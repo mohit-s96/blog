39b0a8d5ba21148878fd1bacd34b9f46b711c73d
msx47 <mohitsrivastava67@gmail.com>
2021-11-21 23:46:33 +0530

    added: dynamic api route for easy caching on the frontend with params. new cache is marked to expire in 4 days

---
+++ src/pages/api/search/[key].ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { fetchSearchQuery } from "../../../../lib/database/getBlogs";
+import { useCors } from "../../../../lib/middleware/corsMW";
+import { createClient } from "redis";
+import { transformRedisKey } from "../../../../util/misc";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    await useCors(req, res);
+
+    const client = createClient({
+      url: process.env.REDIS_ENDPOINT_URI as string,
+      password: process.env.REDIS_PASSWORD,
+    });
+
+    await client.connect();
+
+    const key = req.query.key as string;
+
+    const cached = await client.get(transformRedisKey(key));
+
+    if (cached === null) {
+      const response = await fetchSearchQuery(key);
+      await client.setEx(
+        transformRedisKey(key),
+        345600, // 4 days
+        JSON.stringify(response)
+      );
+      await client.quit();
+      res.status(200).json(response);
+    } else {
+      await client.quit();
+
+      res.status(200).json(cached);
+    }
+  } catch (err) {
+    res.status(500).json({ statusCode: 500, message: (err as any).message });
+  }
+};
+
+export default handler;
