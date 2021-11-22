8bc200acdf2fc728e92a267060cebaf8eca53f1c
msx47 <mohitsrivastava67@gmail.com>
2021-11-22 15:45:43 +0530

    added: search results are now links

--- src/components/search-blogs/searchResults.tsx
+++ src/components/search-blogs/searchResults.tsx
@@ -1,22 +1,29 @@
 import { format } from "date-fns";
-import React, { ReactElement } from "react";
+import React, { Dispatch, ReactElement, SetStateAction } from "react";
 import { BlogSlug } from "../../../types/blogtypes";
 import { ThemeType } from "../../../types/globalTypes";
 import Tags from "../tags/Tags";
+import Link from "next/link";
 
 interface Props {
   data: Partial<BlogSlug>;
+  setVisible: Dispatch<SetStateAction<boolean>>;
 }
 
-function SearchResults({ data }: Props): ReactElement {
+function SearchResults({ data, setVisible }: Props): ReactElement {
   return (
     <div
       className={`p-2 m-2 flex flex-col w-[98%] mx-auto rounded-2xl dark:bg-primary-bg-dark bg-primary-bg-light`}
     >
       <div className="flex justify-between">
-        <p className="p-2 text-xl text-primary-accent-dark text-sm-res padding-0-res">
-          {data.title}
-        </p>
+        <Link href={`/blog/${data.uri}`}>
+          <p
+            className="p-2 text-xl text-primary-accent-dark text-sm-res padding-0-res cursor-pointer"
+            onClick={() => setVisible(false)}
+          >
+            {data.title}
+          </p>
+        </Link>
         <p className="p-2 text-xl text-primary-accent-dark text-sm-res padding-0-res">
           {format(data.createdAt as number, "do MMM, yy")}
         </p>
