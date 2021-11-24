72e08bd5347e79cfb059e5a868447c25ba336e5b
msx47 <mohitsrivastava67@gmail.com>
2021-11-24 19:26:56 +0530

    fixed: related blogs width issue

--- src/components/related-bar/readingList.tsx
+++ src/components/related-bar/readingList.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React, { ReactElement } from "react";
 import { BlogSlug, RelatedBlogsType } from "../../../types/blogtypes";
 import { ThemeType } from "../../../types/globalTypes";
@@ -9,7 +10,7 @@ interface Props {
 
 function ReadingList({ theme, list }: Props): ReactElement {
   return (
-    <div className="flex flex-col w-10/12">
+    <div className="flex flex-col w-10/12 min-w-[300px]">
       <p className="p-1 2xl:p-2 bg-primary-accent-light font-bold rounded-md text-white text-sm 2xl:text-lg">
         related blogs:
       </p>
@@ -19,11 +20,11 @@ function ReadingList({ theme, list }: Props): ReactElement {
           key={blog._id as any}
         >
           <p
-            className={`hover:underline p-2 text-sm 2xl:text-lg cursor-pointer font-bold dark:text-primary-bg-light text-gray-800`}
+            className={`hover:underline p-2 w-full text-sm 2xl:text-lg cursor-pointer font-bold dark:text-primary-bg-light text-gray-800`}
           >
-            {blog.title}
+            <Link href={`/blog/${blog.uri}`}>{blog.title}</Link>
           </p>
-          <div className="flex mt-2">
+          <div className="flex mt-2 overflow-hidden">
             {(blog as BlogSlug).tags.map((tag) => (
               <span
                 key={tag}
